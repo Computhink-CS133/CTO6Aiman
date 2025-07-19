9ffa0f496e26822f9745fe730144d263ced8b581
Computhink-CS133 <cs133@computhink.com.sg>
2025-07-19 07:09:29 +0000

    Jul 19, 2025, 7:09 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -14,7 +14,7 @@ ellipse(500,500,500,500)
 ellipse(500,600,300,100)
 circle(400,400,90)
 circle(600,400,90)
-circle(300,300,200)
+circle(300,200,200)
 
   // Challenge 2: A face using circle() function
 
