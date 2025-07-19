9007c5b319ca06af9d19d39c07d6d432dfc71695
Computhink-CS133 <cs133@computhink.com.sg>
2025-07-19 07:23:07 +0000

    Jul 19, 2025, 7:23 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -18,7 +18,7 @@ function draw() {
 // circle(700,212,200);
 // fill(255,0,255);
 //Bob stays
-triangle(2,125,300,300,600,300)
+triangle(500,125,300,300,600,300)
 
   // Challenge 2: A face using circle() function
 
