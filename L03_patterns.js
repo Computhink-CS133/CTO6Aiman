8862d24338d6e81dabf72297f6fae009adbf9363
Computhink-CS133 <cs133@computhink.com.sg>
2025-07-26 06:16:47 +0000

    Jul 26, 2025, 6:16 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -8,7 +8,7 @@ function draw() {
   stroke(1000,1000,1000,1000);
   circle(Math.random()*600,Math.random()*400,100);
   circle(Math.random()*600,Math.random()*400,100);
-  fill(Math.random());
+  fill(Math.random()*255,);
 
 
   // Task 1: Colour Gradient
