54a711fac396aca86e5f9d580d2fd98f528535fe
Computhink-CS133 <cs133@computhink.com.sg>
2025-07-26 06:40:43 +0000

    Jul 26, 2025, 6:40 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,7 +6,7 @@ function setup() {
   
 function draw() {
   
-  for (var i=0; i<6 ;i++);
+  for (var i=0; i<6 ;i++)
   {
   circle(50+(i*50),200,100);
   fill(Math.random()*200,Math.random()*255,Math.random()*255);
