563ead05acf4f2c2c7c2c49fff8da81303b75831
Computhink-CS133 <cs133@computhink.com.sg>
2025-07-26 06:12:13 +0000

    Jul 26, 2025, 6:12 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -5,8 +5,8 @@ function setup() {
   }
   
 function draw() {
-  stroke(220,1000,1000,1000)
-  rect(300,200,50,50)
+  stroke(1000,1000,1000,1000);
+  rect(300,200,50,50);
 
 
   // Task 1: Colour Gradient
