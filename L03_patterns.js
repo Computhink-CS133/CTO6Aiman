7008849afd141410959f79f0360c3e36d0a38531
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-02 07:09:57 +0000

    Aug 2, 2025, 7:09 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,43 +1,66 @@
-var spacehahah=120
-var num=8
+// var spacehahah=120
+// var num=10
 
 function setup() {
 
-  createCanvas((spacehahah*num)*2, spacehahah*num);
+  createCanvas(600,1000);//(spacehahah*num)*2, spacehahah*num
   background(100);
   // noLoop(); // Stops continuous drawing
   }
   
-  var y=200
-  var colorvalue=255;
-  for (var l=0;l<num ;l++)
-  {
+// //   var y=200;
+// //   var colorvalue=255;
+// //   for (var l=0;l<num ;l++);
+// //   {
 function draw() 
   {
-    for (var i=0;i<num ;i++)
-  {
-  var result=(i%2)
-  stroke(1000,1000,1000,1000);
-  rect(spacehahah+(i*100)+spacehahah,y,100,100);
+var circlediameter=30;
+var rowcircle=20;
+var totalwidth=circlediameter*rowcircle;
 
-  if (result===1)
-  {
-    colorvalue=255;
-  }
-  else
-  {
-    colorvalue=0
-  }
-  fill(colorvalue)
-  }
 
-  // Task 1: Colour Gradient
+var startx= (width - totalwidth)/2+circlediameter/2;
+var starty=(height - totalwidth)/2 + circlediameter/2;
+for (var row=0;row<rowcircle ;row++)
+{
+for (var l=0;l<rowcircle ;l++){
+  var x = startx + l * circlediameter;
+  var y = starty+row *circlediameter
+  fill(0,0,0);
+  circle(x,y,circlediameter,circlediameter);
 
-  // Task 2: Colour Loop
 
-  // Task 3: Row of Circles
 
-  // Task 4: Grid of Circles
 }
 
+}
+//     rect(width/2,height/2,height,width)
+//     for (var i=0;i<num ;i++)
+//   {
+//   var result=(i%2);
+//   stroke(1000,1000,1000,1000);
+//   rect(spacehahah+(i*100)+spacehahah,y,100,100);
+
+//   if (result===1)
+//   {
+//     colorvalue=255;
+//   }
+//   else
+//   {
+//     colorvalue=0;
+//   }
+//   fill(colorvalue);
+//   }
+
+//   // Task 1: Colour Gradient
+
+//   // Task 2: Colour Loop
+
+//   // Task 3: Row of Circles
+
+//   // Task 4: Grid of Circles
+// }
+
   }
+
+
