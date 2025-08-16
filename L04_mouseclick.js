8e1900dbf31b36df750c93eb6caab65ddff388b7
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-16 06:40:33 +0000

    Aug 16, 2025, 6:40 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -19,8 +19,8 @@ function mousePressed(){
     r=Math.random()*255;
     g=Math.random()*255;
     b=Math.random()*255;
-    var y = Math.random()*1000;
-    var y = Math.random()*1000;
+ y = Math.random()*1000;
+     x = Math.random()*1000;
 }
 
 function mouseReleased(){
