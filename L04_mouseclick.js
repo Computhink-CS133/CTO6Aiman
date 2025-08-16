54c9dc1d0607d9975913f3c34b8f1874ebba6183
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-16 06:39:13 +0000

    Aug 16, 2025, 6:39 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -8,7 +8,8 @@ function setup() {
 var r=Math.random()*255;
 var g=Math.random()*255;
 var b=Math.random()*255;
-var x=Math.random()*
+var x=Math.random()*1000;
+var y = 
 function draw(){
     fill(r,g,b);
     ellipse(500,500,500,500);
