a399df78505cba9927bb2ac078026f2827ac272c
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-16 07:21:03 +0000

    Aug 16, 2025, 7:21 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -998,36 +998,36 @@ function setup() {
 
 
 
-// var r=Math.random()*255;
-// var g=Math.random()*255;
-// var b=Math.random()*255;
-// var x=Math.random()*1000;
-// var y = Math.random()*1000;
-// var cor1=Math.random()*1000;
-// var cor2=Math.random()*1000;
-// var cor3=Math.random()*1000;
-// function draw(){
-//     fill(r,g,b);
-//     if (mousePressed){
-//     triangle(cor1,cor2,cor3,x,y);
-// }
-// }
+var r=Math.random()*255;
+var g=Math.random()*255;
+var b=Math.random()*255;
+var x=Math.random()*1000;
+var y = Math.random()*1000;
+var cor1=Math.random()*1000;
+var cor2=Math.random()*1000;
+var cor3=Math.random()*1000;
+function draw(){
+    fill(r,g,b);
+    if (mousePressed){
+    triangle(cor1,cor2,cor3,x,y);
+}
+}
 
-// function mousePressed(){
-//     r=Math.random()*255;
-//     g=Math.random()*255;
-//     b=Math.random()*255;
-//      y = Math.random()*1000;
-//      x = Math.random()*1000;
-//     cor1=Math.random()*1000;
-//     cor2=Math.random()*1000;
-//     cor3=Math.random()*1000;
-//     triangle.visibility = visible;
+function mousePressed(){
+    r=Math.random()*255;
+    g=Math.random()*255;
+    b=Math.random()*255;
+     y = Math.random()*1000;
+     x = Math.random()*1000;
+    cor1=Math.random()*1000;
+    cor2=Math.random()*1000;
+    cor3=Math.random()*1000;
+    triangle.visibility = visible;
      
-// }
-// function mouseReleased(){
-//     triangle.visibility = hidden;
-// }
+}
+function mouseReleased(){
+    triangle.visibility = hidden;
+}
 
 
 
