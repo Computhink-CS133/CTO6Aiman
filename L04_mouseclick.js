c648f73a28462faad7da4ace484e6d3bbf1665a7
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-16 07:24:23 +0000

    Aug 16, 2025, 7:24 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1017,8 +1017,9 @@ function mousePressed(){
     cor1=Math.random()*1000;
     cor2=Math.random()*1000;
     cor3=Math.random()*1000;
+    
     fill(r,g,b);
-    triangle(cor1,cor2,cor3,x,y);
+    triangle(cor1,cor2,cor3,x,y,);
     triangle.visibility = false;
 
      
