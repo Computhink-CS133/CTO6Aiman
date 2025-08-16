cd7ebfdbe9f482f6e1eb77870599d3481ea095df
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-16 06:55:02 +0000

    Aug 16, 2025, 6:55 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -7,14 +7,11 @@ function setup() {
 
 let shapeColor ="blue";
 
-function setup(){
-createCanvas(400,400)
-background(200);
-}
+
 
 function draw(){
 fill(shapeColor);
-ellipse(200,200,80,80)
+ellipse(500,200,80,80)
 }
 
 function mousePressed(){
