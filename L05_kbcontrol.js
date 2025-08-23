cc4c3fe813a286c2dddcacc0108f2d3dd7a84238
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-23 06:25:44 +0000

    Aug 23, 2025, 6:25 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -3,9 +3,9 @@ function setup() {
   createCanvas(1000,1000);
   background(100);
 }
-var size=5
+var size=5;
 function mouseDragged()
 {
-    fill(Math.random()*255,Math.random()*255,Math.random()*255,)
-    ellipse(mouseX,mouseY,size,size)
+    fill(Math.random()*255,Math.random()*255,Math.random()*255);
+    ellipse(mouseX,mouseY,size,size);
 }
