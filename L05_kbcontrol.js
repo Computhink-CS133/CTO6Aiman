5cd9e7c0c17b100496619e2417c963e66f7f0eda
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-23 07:07:53 +0000

    Aug 23, 2025, 7:07 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,18 +1,18 @@
-function setup()
-{
-  createCanvas(1000,1000);
-  background(100);
-  noStroke()
-}
-var size=5;
-function draw(){}
-    function mouseDragged()
-{
-    ellipse(mouseX,mouseY,size,size);
-    size+=0.2;
-}
+// function setup()
+// {
+//   createCanvas(1000,1000);
+//   background(100);
+//   noStroke()
+// }
+// var size=5;
+// function draw(){}
+//     function mouseDragged()
+// {
+//     ellipse(mouseX,mouseY,size,size);
+//     size+=0.2;
+// }
 
-    function mousePressed(){
-    size=5;
-    fill(Math.random()*255,Math.random()*255,Math.random()*255);
-}
+//     function mousePressed(){
+//     size=5;
+//     fill(Math.random()*255,Math.random()*255,Math.random()*255);
+// }
