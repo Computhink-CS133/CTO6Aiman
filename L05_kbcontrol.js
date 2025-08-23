8df47cc04bfd71485bf5009869cad04229410ba4
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-23 07:30:11 +0000

    Aug 23, 2025, 7:30 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -521,7 +521,7 @@ function setup()
 }
 var rectsize=50;
 function draw(){
-rect(width/2 - rectsize/2,width/2 - rectsize/2,rectsize,rectsize);
+rectangle(width/2 - rectsize/2,width/2 - rectsize/2,rectsize,rectsize);
 background(100);
 }
 function keyPressed(){
