8d923efdab9482a44093ecea76ec141e8a4aac83
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-30 07:27:57 +0000

    Aug 30, 2025, 7:27 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -527,7 +527,7 @@ function draw(){
 }
 function keyPressed(){
     for(var i = 0;i<Math.random()*255;i++){
-        
+        fill(Ma)
         circle(Math.random()*1000,Math.random()*1000,90)
     }
 }
