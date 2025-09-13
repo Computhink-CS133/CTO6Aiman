93fc6d1e1d3a3af0c7564424401f6a5f798aecb7
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-13 07:08:31 +0000

    Sep 13, 2025, 7:08 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,54 +1,54 @@
-var xvel=0;
-var yvel=0;
-var speed=0.1;
-var rectsize = 10;
-var colour = 50;
+// var xvel=0;
+// var yvel=0;
+// var speed=0.1;
+// var rectsize = 10;
+// var colour = 50;
 
 
-var x = 200;
-var y = 200;
+// var x = 200;
+// var y = 200;
 
-function setup(){
-  createCanvas(400,400);
-  background(100);
-}
-function draw() {
+// function setup(){
+//   createCanvas(400,400);
+//   background(100);
+// }
+// function draw() {
   
-  background(100);
-  if(keyIsDown(RIGHT_ARROW ))
-  {
+//   background(100);
+//   if(keyIsDown(RIGHT_ARROW ))
+//   {
     
-    xvel+=speed;
-  }
+//     xvel+=speed;
+//   }
 
-  if(keyIsDown(LEFT_ARROW ))
-  {
-    xvel-=speed;
-  }
-    if(keyIsDown(DOWN_ARROW ))
-  {
+//   if(keyIsDown(LEFT_ARROW ))
+//   {
+//     xvel-=speed;
+//   }
+//     if(keyIsDown(DOWN_ARROW ))
+//   {
   
-    yvel+=speed;
-  }
+//     yvel+=speed;
+//   }
 
-  if(keyIsDown(UP_ARROW ))
-  {
+//   if(keyIsDown(UP_ARROW ))
+//   {
    
-    yvel-=speed;
-  }
-rect(x - rectsize,y- rectsize,rectsize,rectsize);
+//     yvel-=speed;
+//   }
+// rect(x - rectsize,y- rectsize,rectsize,rectsize);
 
-if(keyIsDown===32){
-    colour = 200;
-}
-else{
-    colour = 50;
-}
-  x+=xvel;
-  y+=yvel;
-  xvel*=0.9;
-  yvel*=0.9;
-  x=constrain(x,100,300);
-  y=constrain(y,100,300);
+// if(keyIsDown===32){
+//     colour = 200;
+// }
+// else{
+//     colour = 50;
+// }
+//   x+=xvel;
+//   y+=yvel;
+//   xvel*=0.9;
+//   yvel*=0.9;
+//   x=constrain(x,100,300);
+//   y=constrain(y,100,300);
   
-}
+// }
