1d7a7478cbee56d90025cc02b6252bccd5d87da8
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-13 07:03:45 +0000

    Sep 13, 2025, 7:03 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -35,7 +35,7 @@ function draw() {
    
     yvel-=speed;
   }
-rect(x - rectsize/2,y- rectsize/2,rectsize,rectsize)
+rect(x/2 - rectsize/2,y/2- rectsize/2,rectsize,rectsize)
  
   x+=xvel;
   y+=yvel;
