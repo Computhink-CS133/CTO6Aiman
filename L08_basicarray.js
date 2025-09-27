9ec73db62905f07b387c28530e062e1bfdb0d739
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 07:02:30 +0000

    Sep 27, 2025, 7:02 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -20,7 +20,7 @@ rect(squarex - squareSize/2,squarey - squareSize/2,squareSize,squareSize);
 if (squarex - squareSize/2 <=0 || squarex + squareSize/2 >= width ){
     squarexvel *= -1;
     fill(Math.random()*255,Math.random()*255,Math.random()*255);
-    squarexvel
+    squarexvel*=2;
 
 }
 
