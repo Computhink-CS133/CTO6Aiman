20610d906605b4a2eae4bf60169e04f7cc1dab6e
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 07:25:04 +0000

    Sep 27, 2025, 7:25 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,7 +10,9 @@ createCanvas(6000,4000);
 background(100);
 textSize(200);
 let favfood=["Monke","Cavemen","Dog","Grep","alep","langur","mus","pizarea","piizaman","colddog"]
-for (let i=0;i<favfood.length;i++)
+for (let i=0;i<favfood.length;i++){
+    text(favfood[i],100,50*i)
+}
 }
 
 
