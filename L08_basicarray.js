63abd12a9431fba688b3f0df120fb88fe6ec8dcf
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 06:50:24 +0000

    Sep 27, 2025, 6:50 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -14,7 +14,7 @@ function draw(){
 noStroke();
 ballx+=ballxvel;
 bally+=ballyvel;
-rect(ballx - ballSize/2,bally,ballSize);
+rect(ballx - ballSize/2,bally - ballSize/2,ballSize);
 if (ballx - ballSize/2 <=0 || ballx + ballSize/2 >= width ){
     ballxvel *= -1;
     fill(Math.random()*255,Math.random()*255,Math.random()*255)
