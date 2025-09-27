3a4f152d3862d3f18720ab47c6ea6fed75824eb4
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 06:44:07 +0000

    Sep 27, 2025, 6:44 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -25,11 +25,11 @@ if (bally - ballSize/2 <=0 || bally + ballSize/2 >= height){
 }
 function keyPressed(){
     if (keyCode===UP_ARROW){
-        ballxvel*=1.1;
-        ballyvel*=1.1;
+        ballxvel*=2;
+        ballyvel*=2;
     }
     if (keyCode===DOWN_ARROW){
-        ballxvel*=0.9;
+        ballxvel*=0.5;
         ballyvel*=0.9;
     }
 console.log("Speed: "+ Math.abs(ballxvel))
