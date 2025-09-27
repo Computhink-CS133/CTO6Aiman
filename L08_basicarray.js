d45f9d88b9565815d5019de6722808a975ad52f4
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 06:49:54 +0000

    Sep 27, 2025, 6:49 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,3 +1,37 @@
 // write your codes here
 //n0 :)
 let obj={k:1,c:2,d:3}
+let ballx=300;
+let bally =200;
+const ballSize=30;
+let ballxvel=2;
+let ballyvel=2;
+function setup(){
+createCanvas(6000,4000);
+background(200);
+}
+function draw(){
+noStroke();
+ballx+=ballxvel;
+bally+=ballyvel;
+circle(ballx,bally,ballSize);
+if (ballx - ballSize/2 <=0 || ballx + ballSize/2 >= width ){
+    ballxvel *= -1;
+    fill(Math.random()*255,Math.random()*255,Math.random()*255)
+}
+if (bally - ballSize/2 <=0 || bally + ballSize/2 >= height){
+    ballyvel *= -1;    
+    fill(Math.random()*255,Math.random()*255,Math.random()*255)
+}
+}
+function keyPressed(){
+    if (keyCode===UP_ARROW){
+        ballxvel*=2;
+        ballyvel*=2;
+    }
+    if (keyCode===DOWN_ARROW){
+        ballxvel*=0.5;
+        ballyvel*=0.5;
+    }
+console.log("Speed: "+ Math.abs(ballxvel))
+}
