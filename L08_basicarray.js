53a4ad3e8d8fe899965f4bcc30dc4ae5de918dcb
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 07:02:15 +0000

    Sep 27, 2025, 7:02 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -4,7 +4,7 @@ let obj={k:1,c:2,d:3}
 let squarex=300;
 let squarey =200;
 const squareSize=30;
-let squarexvel=50;
+let squarexvel=2;
 let squareyvel=0;
 function setup(){
 createCanvas(6000,300);
@@ -20,6 +20,7 @@ rect(squarex - squareSize/2,squarey - squareSize/2,squareSize,squareSize);
 if (squarex - squareSize/2 <=0 || squarex + squareSize/2 >= width ){
     squarexvel *= -1;
     fill(Math.random()*255,Math.random()*255,Math.random()*255);
+    squarexvel
 
 }
 
