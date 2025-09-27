188dd1d733c4485358d9c1afa61d5fbfff491309
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 07:15:47 +0000

    Sep 27, 2025, 7:15 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -6,7 +6,7 @@
 // const squareSize=30;
 // let squarexvel=2;
 function setup(){
-createCanvas(600,400);
+createCanvas(6000,4000
 background(100);
 textSize(200);
 text(" Hello imqwertyuiopasdfghjklzxcvbnm",50,50);
