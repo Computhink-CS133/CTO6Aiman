496b0be7405d4c2f92286adb6d11ca702fb063b8
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 06:09:18 +0000

    Sep 27, 2025, 6:09 AM

---
+++ L07_bouncingball.js
@@ -0,0 +1,17 @@
+// write your codes here
+//no :)
+let ballx=300;
+let bally =200;
+const ballSize=30;
+let ballxvel=2;
+let ballyvel=2;
+function setup(){
+createCanvas(600,400);
+background(200);
+}
+function draw(){
+ballx+=ballxvel;
+bally+=ballyvel;
+background(200);
+circle(ballx,bally,ballSize);
+}
