94ad12f76bdfeec3a0375ec28d52443054b820ac
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 06:50:35 +0000

    Sep 27, 2025, 6:50 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -5,7 +5,7 @@ let ballx=300;
 let bally =200;
 const ballSize=30;
 let ballxvel=2;
-let ballyvel=2;
+let ballyvel=0;
 function setup(){
 createCanvas(6000,4000);
 background(200);
@@ -14,7 +14,7 @@ function draw(){
 noStroke();
 ballx+=ballxvel;
 bally+=ballyvel;
-rect(ballx - ballSize/2,bally - ballSize/2,ballSize);
+rect(ballx - ballSize/2,bally - ballSize/2,ballSize,ballSize);
 if (ballx - ballSize/2 <=0 || ballx + ballSize/2 >= width ){
     ballxvel *= -1;
     fill(Math.random()*255,Math.random()*255,Math.random()*255)
