fb5941333ec9e0e6a4e9db1e8f91d7f2270ff00d
Computhink-CS133 <cs133@computhink.com.sg>
2025-09-27 06:24:22 +0000

    Sep 27, 2025, 6:24 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -3,8 +3,8 @@
 let ballx=300;
 let bally =200;
 const ballSize=30;
-let ballxvel=10;
-let ballyvel=10;
+let ballxvel=50;
+let ballyvel=50;
 function setup(){
 createCanvas(600,400);
 background(200);
