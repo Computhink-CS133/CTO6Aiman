4f63a01437c4570d4524dfbcee885902043c00e0
Computhink-CS133 <cs133@computhink.com.sg>
2025-10-04 06:16:24 +0000

    Oct 4, 2025, 6:16 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,13 +10,13 @@ createCanvas(6000,4000);
 background(100);
 textSize(200);
 textAlign(CENTER,CENTER);
-text(favfood[i],100,10*i);
+text(favfood,100,10*i);
 }
 function draw(){
-let favfood=["Monke","Cavemen","Dog","Grep","langur","mus","pizarea","piizaman","colddog"];
-for (let i=0;i<favfood.length;i++){
-    text(favfood[i],100,10*i);
-}
+// let favfood=["Monke","Cavemen","Dog","Grep","langur","mus","pizarea","piizaman","colddog"];
+// for (let i=0;i<favfood.length;i++){
+//     text(favfood[i],100,10*i);
+// }
 }
 
 
