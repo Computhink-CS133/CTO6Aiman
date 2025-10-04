1de1d058c0127fff97c4064fbe95ea974af28812
Computhink-CS133 <cs133@computhink.com.sg>
2025-10-04 06:43:03 +0000

    Oct 4, 2025, 6:43 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -14,3 +14,22 @@ let storyText = [
 "",
 "May the Code be with you!"
 ];
+function setup(){
+createCanvas(6000,4000);
+background(100);
+textSize(500);
+textAlign(LEFT,TOP);
+}
+let movepos=0
+function draw(){
+
+background(100);
+for(let i=0;i<favfood.length;i++){
+  
+    text(favfood[i],50+movepos,500*i+movepos);
+
+  
+}
+movepos+=1
+
+}
