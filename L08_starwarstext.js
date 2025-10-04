dbe3326951825449a61f656df00fc5091d505246
Computhink-CS133 <cs133@computhink.com.sg>
2025-10-04 07:07:43 +0000

    Oct 4, 2025, 7:07 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -37,9 +37,13 @@ background(100);
 push();
 translate(width/2,movepos);
 scale(1.5,2);
-if (started)
+if (started){
 for(let i=0;i<storyText.length;i++){
     text(storyText[i],0,50*i+movepos);
+}
+}
+else{
+    
 }
 
 pop();
