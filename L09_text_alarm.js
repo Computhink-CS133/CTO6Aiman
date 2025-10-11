7e9e70ba1f3d9031f6e751629023ebe00fea8ddd
Computhink-CS133 <cs133@computhink.com.sg>
2025-10-11 06:43:20 +0000

    Oct 11, 2025, 6:43 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -17,7 +17,8 @@ function settimer(){
     return timer
 }
 function mousePressed(){
-     let  countdown = setInterval(settimer,1000);
+     let countdown = setInterval(settimer,1000);
+     global countdown
      
 
 }
