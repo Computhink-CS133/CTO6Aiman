a5ca2877fd9a918a86d6276255dfc9f49a6f709a
Computhink-CS133 <cs133@computhink.com.sg>
2025-10-11 06:09:48 +0000

    Oct 11, 2025, 6:09 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -3,29 +3,30 @@
 
 
 
-function setup(){
-createCanvas(1000,800);
-background(100);
-textSize(50);
-textAlign(CENTER,CENTER);
-}
-let xpos=0
-let xspeed=0
-function draw(){
+// function setup(){
+// createCanvas(1000,800);
+// background(100);
+// textSize(50);
+// textAlign(CENTER,CENTER);
+// }
+// let xpos=0
+// let xspeed=0
+// function draw(){
 
-background(100);
+// background(100);
 
-// push();
-// translate(xpos,height/2)
+// // push();
+// // translate(xpos,height/2)
 
-text("Bounce",width/2 +xpos,height/2)
-if (xpos<-500){
-xspeed=-10
-}
-xpos-=xspeed
-xspeed+=0.1
-// pop();
+// text("Bounce",width/2 +xpos,height/2)
+// if (xpos<-500){
+// xspeed=-10
+// }
+// xpos-=xspeed
+// xspeed+=0.1
+// // pop();
 
 
-}
+// }
+
 
