caac4ae7ccaa3c98c2e35ea9c2f2b118b25ca69c
Computhink-CS133 <cs133@computhink.com.sg>
2025-10-11 06:27:25 +0000

    Oct 11, 2025, 6:27 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -35,10 +35,11 @@ function draw(){
 let h = hour();
 let m = minute();
 let s = second();
+
 let time = nf(h,2)+ ":" + nf(m,2) + ":" + nf(s,2);
 fill(0);
 textSize(50);
 text(500,400,time);
 background(100);
-console.log(time)
+console.log(time);
 }
