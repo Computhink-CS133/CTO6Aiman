b0d1feb3f371e65c6ac6755a861fc7d2ac25efb8
Computhink-CS133 <cs133@computhink.com.sg>
2025-10-18 07:16:51 +0000

    Oct 18, 2025, 7:16 AM

--- L10_name.js
+++ L10_name.js
@@ -7,5 +7,6 @@ function setup(){
     createCanvas(400,400);
     inputtext=createInput();
     background(100);
-    inputtext.position(200,200)
+    inputtext.position(100,300)
 }
+function draw(){}
