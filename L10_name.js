858ea3af0461f0776d61c5f5a16d545fd7356005
Computhink-CS133 <cs133@computhink.com.sg>
2025-10-18 06:39:13 +0000

    Oct 18, 2025, 6:39 AM

--- L10_name.js
+++ L10_name.js
@@ -17,7 +17,7 @@ background(bgcolour1,bgcolour2,bgcolour3);
 console.log(countdown);
 }
 function countdownfunc(){
-    if (countdown>0){
+    if (countdown>0&& co){
     bgcolour1=Math.random()*255;
     bgcolour2=Math.random()*255;
     bgcolour3=Math.random()*255;
