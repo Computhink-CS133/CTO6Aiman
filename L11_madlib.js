15cc72277d56cfe6fe96ada36153d19ba852b0b5
Computhink-CS133 <cs133@computhink.com.sg>
2025-11-01 07:21:49 +0000

    Nov 1, 2025, 7:21 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -6,7 +6,7 @@ function setup() {
     inputField = createInput();
     inputField.position(200,height-80);
     submitButton = createButton("Show");
-    submitButton.position(inputField.x,inputField.y)
+    submitButton.position(inputField.x+1,inputField.y)
     submitButton.mousePressed(displayInput);
 }
 function draw() {
