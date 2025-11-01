4a28c26a2d53642264f01fa91d3784405246bab2
Computhink-CS133 <cs133@computhink.com.sg>
2025-11-01 07:03:42 +0000

    Nov 1, 2025, 7:03 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,8 +1,8 @@
 let inputField;
 let sumbit;
 function setup() {
-    createCanvas(600,400)
-    background(100)
+    createCanvas(600,400);
+    background(100);
     inputField = createInput();
     inputField.position(200,height-80);
     inputField.input(updatetext);
