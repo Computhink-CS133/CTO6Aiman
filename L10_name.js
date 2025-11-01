f625674c2f98c68f5cc1331341975d35363ff05c
Computhink-CS133 <cs133@computhink.com.sg>
2025-11-01 06:38:46 +0000

    Nov 1, 2025, 6:38 AM

--- L10_name.js
+++ L10_name.js
@@ -44,7 +44,7 @@ function draw() {
     text("Pick a colour",20,50);
     fill(255);
     rect(50,100,500,160,50,50,50,50);
-    text(usertext,550,260);
+    text(usertext,300,200);
     
 }
 function updatetext() {
