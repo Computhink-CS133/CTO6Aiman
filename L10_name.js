7d7e6c9ad23530be266c98658578da0831185e6c
Computhink-CS133 <cs133@computhink.com.sg>
2025-11-01 06:08:27 +0000

    Nov 1, 2025, 6:08 AM

--- L10_name.js
+++ L10_name.js
@@ -2,21 +2,22 @@
 //     createCanvas(400,400);
 
 // }
-let inputtext,updatetext;
-function setup(){
-    createCanvas(400,400);
-    inputtext=createInput();
-    background(100);
-    inputtext.position(100,300);
-    inputtext.input(updatetext);
-}
-function draw(){
-    textsize(28)
-    updatethetext(updatetext)
-    background(100);
-    text(updatetext,200,200);
+// let inputtext,updatetext;
+// function setup(){
+//     createCanvas(400,400);
+//     inputtext=createInput();
+//     background(100);
+//     inputtext.position(100,300);
+//     inputtext.input(updatetext);
+// }
+// function draw(){
+//     textsize(28)
+//     updatethetext(updatetext)
+//     background(100);
+//     text(updatetext,200,200);
 
-}
-function updatethetext(text) {
-    updatetext=inputtext
-}
+// }
+// function updatethetext(text) {
+//     updatetext=inputtext
+// }
+let na
