0d61bee4f58fb5b0e1e516a6f3e385d07dccf73a
Computhink-CS133 <cs133@computhink.com.sg>
2025-11-01 07:02:06 +0000

    Nov 1, 2025, 7:02 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -3,7 +3,7 @@ let sumbit;
 function setup() {
     createCanvas(600,400)
     background(100)
-        usertext = createInput('Name');
-    usertext.position(200,height-80);
+    inputField = createInput('Name');
+   inputField.position(200,height-80);
     usertext.input(updatetext);
 }
