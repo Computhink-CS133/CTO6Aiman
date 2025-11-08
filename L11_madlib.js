0e75621390ac480cb14dbacb1039873a6c0c960f
Computhink-CS133 <cs133@computhink.com.sg>
2025-11-08 06:21:01 +0000

    Nov 8, 2025, 6:21 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -20,7 +20,8 @@ function setup() {
     submitButton.mousePressed(displayInput);
 }
 function draw() {
-let stowytempwates,stowytext
+let stowytempwates;
+let stowytext="";
     stowytempwates["The adj noun decided to verb at the place.","One day, a adj noun wanted to verb in place.","Did you hear about the adj noun that tried to verb near place."];
 
 
@@ -31,7 +32,7 @@ function displayInput() {
     // console.log(adj.value());
     // console.log(verb.value());
     // console.log(place.value());
-    stowytext
+    stowytext = template
 
 
 
