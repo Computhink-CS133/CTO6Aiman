eba1303b8b7f51bdb79bbd2b1208975f36888bd4
Computhink-CS133 <cs133@computhink.com.sg>
2025-11-08 06:58:14 +0000

    Nov 8, 2025, 6:58 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,5 +1,19 @@
 // write your codes here
 //no🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿
+let 
 function setup() {
-    
+        createCanvas(600,400);
+    background(220);
+  
+    noun = createInput("monkey ");
+    noun.position(200,40);    
+    adj = createInput("disabled");
+    adj.position(200,60);    
+    verb = createInput("pp");
+    verb.position(200,80);    
+    place = createInput("the toilet");
+    place.position(200,100);
+    submitButton = createButton("Show");
+    submitButton.position(place.x+200,place.y)
+    submitButton.mousePressed(displayInput);
 }
