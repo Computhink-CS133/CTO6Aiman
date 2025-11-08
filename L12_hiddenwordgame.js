cbbfb03878a71ff6ebd01f022a604ba22b6d5e24
Computhink-CS133 <cs133@computhink.com.sg>
2025-11-08 07:14:16 +0000

    Nov 8, 2025, 7:14 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -7,7 +7,8 @@ function setup() {
   
     input = createInput("Disabled monkey");
     input.position(100,200);
-    input.size
+    input.size(150,30);
+    input.
     sumbit = createButton("Display");
     sumbit.position(300,200);
     sumbit.mousePressed(displayInput);
